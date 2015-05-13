ff3e4efe0cf8004f2a26685c9ca5446b50058273
lyip1992 <lyip1992@gmail.com>
2015-05-13 16:29:35 -0700

    Fixed typo.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+ var newStack = {storage: {}};
+ _.extend(newStack, stackMethods);
+ return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  counter: 0,
+  push: function(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+
+  },
+  pop: function() {
+    if(this.counter > 0) {
+      var temp = this.storage[this.counter - 1];
+      delete this.storage[this.counter - 1];
+      this.counter--;
+      return temp;
+    }
+  },
+  size: function() {
+    return this.counter;
+  }
+};
+
+//var giraffeStack = Stack();
 
 
