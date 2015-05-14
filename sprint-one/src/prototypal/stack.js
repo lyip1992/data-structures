2d190acddb7b463acc2830ca2224a49cfff7482d
lyip1992 <lyip1992@gmail.com>
2015-05-14 10:07:19 -0700

    Passed tests for stack protypal pattern.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,24 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  return newStack;
 };
 
 var stackMethods = {};
 
-
+stackMethods.count =  0;
+stackMethods.push = function(value){
+  this.storage[this.count] = value;
+  this.count++;
+};
+stackMethods.pop = function(){
+  if(this.count > 0) {
+    var temp = this.storage[this.count - 1];
+    delete this.storage[this.count - 1];
+    this.count--;
+    return temp;
+  }
+};
+stackMethods.size = function(){
+  return this.count;
+};
