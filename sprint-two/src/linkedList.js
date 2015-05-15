d7f4a079d425cbdba5b04b9069b25b89bbc2ce75
lyip1992 <lyip1992@gmail.com>
2015-05-14 20:21:24 -0700

    Passed all tests for implementation of linked list.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+       temp = new Node(value);
+    if (list.tail) {
+      list.tail.next = temp;
+    }
+    list.tail = temp;
+    if (!list.head) {
+      list.head = temp;
+    }
   };
 
   list.removeHead = function(){
+    var temp = list.head.value;
+    list.head = list.head.next;
+    return temp;
   };
 
   list.contains = function(target){
+    var temp = list.head;
+    while (temp) {
+      if (temp.value === target) {
+        return true;
+      }
+      if (temp.next) {
+        temp = temp.next;
+      } else {
+        return false;
+      }
+
+    };
+
+
   };
 
   return list;
