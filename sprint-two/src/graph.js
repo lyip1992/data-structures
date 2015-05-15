340746e9a79474747c574473165a78a44ea3cb95
lyip1992 <lyip1992@gmail.com>
2015-05-15 12:48:15 -0700

    Passed tests for Graph implementation.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,40 @@
 
 
-var Graph = function(){
+var Graph = function() {
+  this.vertices = {};
+  this.edges = {};
 };
 
 Graph.prototype.addNode = function(node){
+  this.vertices[node] = node;
 };
 
 Graph.prototype.contains = function(node){
+  return !!this.vertices[node];
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this.vertices[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  return this.edges[fromNode] === toNode;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this.edges[fromNode] = toNode;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  if (this.edge[fromNode] === toNode) {
+    delete this.edge[fromNode];
+  }
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for (var key in this.vertices) {
+    cb(this.vertices[key]);
+  }
 };
 
 /*
