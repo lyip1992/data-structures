9bf29edc286fda415989196b09e8261202eb1af8
lyip1992 <lyip1992@gmail.com>
2015-05-16 11:25:20 -0700

    Finished hash table basic requirements.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,14 +5,39 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if (i === 0) {
+    if (!this._storage.get(i)) {
+      var temp = new LinkedList();
+      temp.addToTail(v);
+      this._storage.set(i, temp);
+    } else {
+      var temp = this._storage.get(i);
+      temp.addToTail(v);
+      this._storage.set(i, temp);
+    }
+  } else {
+    this._storage.set(i, v);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if (i === 0) {
+    if (this._storage.get(i).contains(k)) {
+      return k;
+    } else {
+      return;
+    }
+  } else {
+    return this._storage.get(i);
+  }
+
 
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i, null);
 
 };
 
