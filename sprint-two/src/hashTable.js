f764149ae9421f43ca1caea4270a957da786a320
Leon Yip <lyip1992@gmail.com>
2015-06-17 23:07:08 -0700

    refactored hash table to use arrays instead of linked list

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,46 +1,88 @@
 var HashTable = function(){
   this._limit = 8;
+  this._size = 0;
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  if (i === 0) {
-    if (!this._storage.get(i)) {
-      var temp = new LinkedList();
-      temp.addToTail(v);
-      this._storage.set(i, temp);
-    } else {
-      var temp = this._storage.get(i);
-      temp.addToTail(v);
-      this._storage.set(i, temp);
-    }
-  } else {
-    this._storage.set(i, v);
-  }
-};
+HashTable.prototype.insert = function(key, value) {
+  var idx = getIndexBelowMaxForKey(key, this._limit);
+  var bucket = this._storage.get(idx);
 
-HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  if (i === 0) {
-    if (this._storage.get(i).contains(k)) {
-      return k;
-    } else {
+  if( !bucket ) {
+    bucket = [];
+    this._storage.set(idx, bucket);
+  }
+
+  for( var i = 0; i < bucket.length; i++ ) {
+    var tuple = bucket[i];
+    if( tuple[0] === key) {
+      tuple[1] = value;
       return;
     }
-  } else {
-    return this._storage.get(i);
   }
 
-
+  bucket.push([ key, value ]);
+  this._size++;
+  if( this._size > 0.75 * this._limit ) {
+    this.resize(this._limit * 2);
+  }
 };
 
-HashTable.prototype.remove = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, null);
+HashTable.prototype.retrieve = function(key) {
+  var idx = getIndexBelowMaxForKey(key, this._limit);
+  var bucket = this._storage.get(idx);
 
+  if( !bucket ) {
+    return null;
+  }
+
+  for( var i = 0; i < bucket.length; i++ ) {
+    var tuple = bucket[i];
+    if( tuple[0] === key ) {
+      return tuple[1];
+    }
+  }
+
+  return null;
 };
 
+HashTable.prototype.remove = function(key) {
+  var idx = getIndexBelowMaxForKey(key, this._limit);
+  var bucket = this._storage.get(idx);
+
+  if( !bucket ) {
+    return null;
+  }
+
+  for( var i = 0; i < bucket.length; i++ ) {
+    var tuple = bucket[i];
+    if( tuple[0] === key ) {
+      bucket.splice(i, 1);
+      this._size--;
+      if( this._size < 0.25 * this._limit) {
+        this.resize( this._limit / 2 );
+      }
+      return tuple[1];
+    }
+  }
+
+  return null;
+};
+
+HashTable.prototype.resize = function(newSize) {
+  var oldStorage = this._storage;
+  this._storage = new LimitedArray(newSize);
+  this._limit = newSize;
+  this._size = 0;
+
+  oldStorage.each(function(bucket) {
+    if( !bucket ) { return; }
+    for( var i = 0; i < bucket.length; i++ ) {
+      var tuple = bucket[i];
+      this.insert(tuple[0], tuple[1]);
+    }
+  }.bind(this));
+};
 
 
 /*
