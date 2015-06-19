b30a6ffbc49c2700a879f23ee81aec98e2494040
Leon Yip <lyip1992@gmail.com>
2015-06-18 21:50:28 -0700

    refactored graph to correct implementation

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,45 +1,60 @@
-
-
 var Graph = function() {
-  this.vertices = {};
-  this.edges = {};
+  this._nodes = {};
 };
 
-Graph.prototype.addNode = function(node){
-  this.vertices[node] = node;
-};
-
-Graph.prototype.contains = function(node){
-  return !!this.vertices[node];
-};
-
-Graph.prototype.removeNode = function(node){
-  delete this.vertices[node];
-};
-
-Graph.prototype.hasEdge = function(fromNode, toNode){
-  return this.edges[fromNode] === toNode;
-};
-
-Graph.prototype.addEdge = function(fromNode, toNode){
-  this.edges[fromNode] = toNode;
-};
-
-Graph.prototype.removeEdge = function(fromNode, toNode){
-  if (this.edges[fromNode] === toNode) {
-    delete this.edges[fromNode];
+Graph.prototype.addNode = function(node) {
+  if( node ) {
+    this._nodes[node] = this._nodes[node] || { edges: [] };
   }
 };
 
+Graph.prototype.contains = function(node) {
+  return !(this._nodes[node] === undefined);
+};
+
+Graph.prototype.removeNode = function(node) {
+  if( this.contains(node) ) {
+    for( var targetNode in this._nodes[node].edges) {
+      this._removeEdge(node, targetNode);
+    }
+    delete this._nodes[node];
+  }
+};
+
+Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this._nodes[fromNode].edges.indexOf(toNode) !== -1;
+};
+
+Graph.prototype.addEdge = function(fromNode, toNode) {
+  if( fromNode === undefined || toNode === undefined ) {
+    return null;
+  }
+  if( this._nodes[fromNode].edges.indexOf(toNode) === -1 ) {
+    this._nodes[fromNode].edges.push(toNode);
+  }
+  if( this._nodes[toNode].edges.indexOf(fromNode) === -1 ) {
+    this._nodes[toNode].edges.push(fromNode);
+  }
+};
+
+Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var spliceIndex = this._nodes[fromNode].edges.indexOf(toNode);
+  if( spliceIndex !== -1 ) {
+    this._nodes[fromNode].edges.splice(spliceIndex, 1);
+  }
+  spliceIndex = this._nodes[toNode].edges.indexOf(fromNode) !== -1;
+  if( spliceIndex !== -1 ) {
+    this._nodes[toNode].edges.splice(spliceIndex, 1);
+  }
+
+};
+
 Graph.prototype.forEachNode = function(cb){
-  for (var key in this.vertices) {
-    cb(this.vertices[key]);
+  for( var node in this._nodes ) {
+    cb(node);
   }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
-
